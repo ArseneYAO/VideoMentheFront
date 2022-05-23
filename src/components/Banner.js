1ff510945d78fd0cc31aa859260730c24826af53
Arsène Yao <yarsene81@gmail.com>
2022-05-23 13:12:38 +0200

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,40 @@
+import logo from '../assets/logo.png'
+import '../styles/Banner.css'
+import {useEffect, useState} from 'react';
+
+
+function Banner() {
+    const title = 'Eole-Project'
+    const [initialState, setInitialState] = useState([])
+
+    useEffect(()=>{
+        fetch('/api/').then(res => {
+            if(res.ok){
+                return res.json()
+            }
+        }).then(jsonResponse => setInitialState(jsonResponse))
+    },[])
+
+    console.log(initialState)
+
+    
+    const [msg,setMsg] = useState('')
+    const handleClick = async () => {
+        const data = await fetch('/api/videos')
+        const json = await data.json()
+        const msg = json.msg
+
+        setMsg(msg)
+    }    
+    
+    return (
+        <div className='ep-banner'>
+            <img src={logo} alt='Eole-Project' className='ep-logo' />
+            <h1 className='ep-title'>{title}</h1>
+            <button onClick={handleClick}>Charger vos vidéos</button>
+            <p>{msg}</p>
+        </div>
+    )
+}
+
+export default Banner
